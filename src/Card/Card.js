40bcf794ca0c8519ca82d0af5e78300dcb07151b
Oliver <olliezdai@gmail.com>
2022-01-14 13:22:43 -0500

    Fixed CSS class naming for Cards

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -4,24 +4,24 @@ import natlogo from './natlogo.png'
 
 function Card() {
   return (
-    <div className="App-card">
-        <div className="App-card-contents">
-            <div className="App-card-left">
-            <img className="App-card-image" src={natlogo} alt="olivers logo"></img>
+    <div className="Card">
+        <div className="Card-contents">
+            <div className="Card-left">
+            <img className="Card-image" src={natlogo} alt="olivers logo"></img>
             </div>
-            <div className="App-card-middle">
-            <div className="App-vertical-line"></div>
+            <div className="Card-middle">
+            <div className="Card-vertical-line"></div>
             </div>
-            <div className="App-card-right">
-            <div className="App-details-container">
-                <div className="App-details-name">
+            <div className="Card-card-right">
+            <div className="Card-details-container">
+                <div className="Card-details-name">
                 Oliver Dai
                 </div>
-                <div className="App-details-item">
-                <a className="App-details-link" href="mailto: olliezdai@gmail.com" target="_blank" rel="noreferrer">olliezdai@gmail.com</a>
+                <div className="Card-details-item">
+                <a className="Card-details-link" href="mailto: olliezdai@gmail.com" target="_blank" rel="noreferrer">olliezdai@gmail.com</a>
                 </div>
-                <div className="App-details-item">
-                <a className="App-details-link" href="https://www.linkedin.com/in/olliezdai/" target="_blank" rel="noreferrer">linkedin.com/in/olliezdai/</a>
+                <div className="Card-details-item">
+                <a className="Card-details-link" href="https://www.linkedin.com/in/olliezdai/" target="_blank" rel="noreferrer">linkedin.com/in/olliezdai/</a>
                 </div>
             </div>
             </div>
