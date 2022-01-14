26cdcd91be5a051696a5fc87162a2b704619bc38
Oliver <olliezdai@gmail.com>
2022-01-14 12:11:38 -0500

    Reorganized into card component

--- src/App/App.js
+++ src/App/App.js
@@ -1,32 +1,10 @@
 import './App.css';
-import natlogo from './natlogo.png'
+import Card from '../Card/Card';
 
 function App() {
   return (
     <div className="App-body">
-      <div className="App-card">
-        <div className="App-card-contents">
-          <div className="App-card-left">
-            <img className="App-card-image" src={natlogo} alt="olivers logo"></img>
-          </div>
-          <div className="App-card-middle">
-            <div className="App-vertical-line"></div>
-          </div>
-          <div className="App-card-right">
-            <div className="App-details-container">
-              <div className="App-details-name">
-                Oliver Dai
-              </div>
-              <div className="App-details-item">
-                <a className="App-details-link" href="mailto: olliezdai@gmail.com" target="_blank" rel="noreferrer">olliezdai@gmail.com</a>
-              </div>
-              <div className="App-details-item">
-                <a className="App-details-link" href="https://www.linkedin.com/in/olliezdai/" target="_blank" rel="noreferrer">linkedin.com/in/olliezdai/</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Card />
     </div>
   );
 }
---
+++ src/Card/Card.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Card.css';
+import natlogo from './natlogo.png'
+
+function Card() {
+  return (
+    <div className="App-card">
+        <div className="App-card-contents">
+            <div className="App-card-left">
+            <img className="App-card-image" src={natlogo} alt="olivers logo"></img>
+            </div>
+            <div className="App-card-middle">
+            <div className="App-vertical-line"></div>
+            </div>
+            <div className="App-card-right">
+            <div className="App-details-container">
+                <div className="App-details-name">
+                Oliver Dai
+                </div>
+                <div className="App-details-item">
+                <a className="App-details-link" href="mailto: olliezdai@gmail.com" target="_blank" rel="noreferrer">olliezdai@gmail.com</a>
+                </div>
+                <div className="App-details-item">
+                <a className="App-details-link" href="https://www.linkedin.com/in/olliezdai/" target="_blank" rel="noreferrer">linkedin.com/in/olliezdai/</a>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Card;
