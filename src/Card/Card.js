a2d1af21cabfccd2a447c7fd1bbdea9e94d99ac2
Oliver <olliezdai@gmail.com>
2022-01-14 15:26:14 -0500

    Flip card functionality

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,32 +1,42 @@
 import React from 'react';
 import './Card.css';
+import FlipCard from '../FlipCard/FlipCard';
 import natlogo from './natlogo.png'
 
 function Card() {
   return (
-    <div className="Card">
-        <div className="Card-contents">
-            <div className="Card-left">
-            <img className="Card-image" src={natlogo} alt="olivers logo"></img>
-            </div>
-            <div className="Card-middle">
-            <div className="Card-vertical-line"></div>
-            </div>
-            <div className="Card-card-right">
-            <div className="Card-details-container">
-                <div className="Card-details-name">
-                Oliver Dai
-                </div>
-                <div className="Card-details-item">
-                <a className="Card-details-link" href="mailto: olliezdai@gmail.com" target="_blank" rel="noreferrer">olliezdai@gmail.com</a>
-                </div>
-                <div className="Card-details-item">
-                <a className="Card-details-link" href="https://www.linkedin.com/in/olliezdai/" target="_blank" rel="noreferrer">linkedin.com/in/olliezdai/</a>
-                </div>
-            </div>
-            </div>
-        </div>
-    </div>
+    <FlipCard>
+      <div className="Card">
+          <div className="Card-contents">
+              <div className="Card-left">
+              <img className="Card-image" src={natlogo} alt="olivers logo"></img>
+              </div>
+              <div className="Card-middle">
+                <div className="Card-vertical-line"></div>
+              </div>
+              <div className="Card-right">
+              <div className="Card-details-container">
+                  <div className="Card-details-name">
+                  Oliver Dai
+                  </div>
+                  <div className="Card-details-item">
+                  <a className="Card-details-link" href="mailto: olliezdai@gmail.com" target="_blank" rel="noreferrer" onClick={(e) => e.stopPropagation()}>olliezdai@gmail.com</a>
+                  </div>
+                  <div className="Card-details-item">
+                  <a className="Card-details-link" href="https://www.linkedin.com/in/olliezdai/" target="_blank" rel="noreferrer" onClick={(e) => e.stopPropagation()}>linkedin.com/in/olliezdai/</a>
+                  </div>
+              </div>
+              </div>
+          </div>
+      </div>
+      <div className="Card">
+          <div className="Card-contents">
+              <div className='Card-back'>
+                idk, I should put something here
+              </div>
+          </div>
+      </div>
+    </FlipCard>
   );
 }
 
